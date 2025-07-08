11f237421bad3af0cb35daa7319cad777bae6ce4
Ja2yee <120127833+Ja2yee@users.noreply.github.com>
2025-07-08 11:23:15 +0000

    conversion

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,15 @@
 console.log("Hello world");
+
+const wajii = "Hello Wajahat"
+
+console.log(wajii );
+
+let w = 10
+console.log(typeof w);
+
+
+let x = String(w)
+
+console.log(x);
+console.log(typeof x);
+
