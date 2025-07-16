8c3b7cb3212d5d8363aa4bb0c129aa77a0809b77
Ja2yee <120127833+Ja2yee@users.noreply.github.com>
2025-07-16 07:49:19 +0000

    changes

--- practice/index.js
+++ practice/index.js
@@ -1,15 +1,5 @@
-console.log("Hello world");
-
-const wajii = "Hello Wajahat"
-
-console.log(wajii );
-
-let w = 10
-console.log(typeof w);
 
 
-let x = String(w)
 
-console.log(x);
-console.log(typeof x);
+
 
---
+++ practice/sum-001/index.js
@@ -0,0 +1,4 @@
+let a = 0.2
+let b = 2.0
+c = a + b
+console.log("Sum is :", c);
